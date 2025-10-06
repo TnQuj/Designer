ebbbd4ddfb7e9567dc5a4c3b4743a137fb6de55f
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 09:30:00 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_13_ ChainofResponsibility/ThaiNgocQui_DPM225464_Real13_ChainofResponsibility/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Real13_ChainofResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Approver larry = new Director();
+            Approver sam = new VicePresident();
+            Approver tammy = new President();
+
+            larry.SetSuccessor(sam);
+            sam.SetSuccessor(tammy);
+
+            Purchase p1 = new Purchase(2034, 350.00, "Supplies");
+            larry.ProcessRequest(p1);
+
+            Purchase p2 = new Purchase(2035, 32590.10, "Project X");
+            larry.ProcessRequest(p2);
+
+            Purchase p3 = new Purchase(2036, 122100.00, "Project Y");
+            larry.ProcessRequest(p3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThaiNgocQui_DPM225464_13_ ChainofResponsibility/ThaiNgocQui_DPM225464_Real13_ChainofResponsibility/Purchase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Real13_ChainofResponsibility
+{
+    public class Purchase
+    {
+        public int Number { get; set; }
+        public double Amount { get; set; }
+        public string Purpose { get; set; }
+
+        public Purchase(int number, double amount, string purpose)
+        {
+            Number = number;
+            Amount = amount;
+            Purpose = purpose;
+        }
+    }
+}
