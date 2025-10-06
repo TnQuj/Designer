c17c123b109401449c45211d9ceaefa6ce6e297e
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 10:20:43 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_02_Biulder/ThaiNgocQui_DPM225464_Real02_Biulder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Real02_Biulder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+
+            VehicleBuilder builder;
+
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
