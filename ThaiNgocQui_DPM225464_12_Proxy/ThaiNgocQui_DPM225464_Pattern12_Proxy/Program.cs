4fbe51b4913da4f9f341f2a83771ab6d09742b90
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 09:25:46 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_12_Proxy/ThaiNgocQui_DPM225464_Pattern12_Proxy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Pattern12_Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThaiNgocQui_DPM225464_12_Proxy/ThaiNgocQui_DPM225464_Real12_Proxy/MathProxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Real12_Proxy
+{
+    public class MathProxy : IMath
+    {
+        private Math math = new Math();
+
+        public double Add(double x, double y) => math.Add(x, y);
+        public double Sub(double x, double y) => math.Sub(x, y);
+        public double Mul(double x, double y) => math.Mul(x, y);
+        public double Div(double x, double y) => math.Div(x, y);
+    }
+}
