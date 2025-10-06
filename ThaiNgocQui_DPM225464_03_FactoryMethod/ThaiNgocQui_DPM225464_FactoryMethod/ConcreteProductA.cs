100055d2f7fa5bb57639e1a3161c1f6407b9fe94
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 10:22:54 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_03_FactoryMethod/ThaiNgocQui_DPM225464_FactoryMethod/ConcreteProductA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Pattern03_FactoryMethod
+{
+    class ConcreteProductA : Product
+    {
+    }
+
+}
---
+++ ThaiNgocQui_DPM225464_03_FactoryMethod/ThaiNgocQui_DPM225464_Real03_FactoryMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Real03_FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document[] documents = {
+                new Resume(),
+                new Report()
+            };
+
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
