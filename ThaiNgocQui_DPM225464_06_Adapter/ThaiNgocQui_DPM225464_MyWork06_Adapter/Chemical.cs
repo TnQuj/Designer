9cc5ecc24e3c24205d262bc23ce96ce8b133818a
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 11:06:59 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_06_Adapter/ThaiNgocQui_DPM225464_MyWork06_Adapter/Chemical.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_MyWork06_Adapter
+{
+    public enum Chemical
+    {
+        Water,
+        Benzene,
+        Ethanol
+    }
+}
---
+++ ThaiNgocQui_DPM225464_06_Adapter/ThaiNgocQui_DPM225464_MyWork06_Adapter/Compound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ThaiNgocQui_DPM225464_MyWork06_Adapter
+{
+    public class Compound
+    {
+        public Chemical Chemical { get; protected set; }
+        public float BoilingPoint { get; protected set; }
+        public float MeltingPoint { get; protected set; }
+        public double MolecularWeight { get; protected set; }
+        public string MolecularFormula { get; protected set; }  // bỏ dấu ?
+
+        public virtual void Display()
+        {
+            Console.WriteLine("\nCompound: Unknown ------ ");
+        }
+    }
+}
