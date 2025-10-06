d20380d819330c3e7a18e23fe93bad5f996a7206
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 08:19:22 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_Prototype/ThaiNgocQui_DPM225464_Pattern04_Prototype/Prototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Pattern04_Prototype
+{
+    abstract class Prototype
+    {
+        public string Id { get; private set; }
+
+        public Prototype(string id)
+        {
+            Id = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+}
