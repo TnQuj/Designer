7dc78ce0dfdec43682ef0918b289763c9848abce
Thai Ngoc Qui <155046579+TnQuj@users.noreply.github.com>
2025-10-06 10:43:46 +0700

    Add files via upload

---
+++ ThaiNgocQui_DPM225464_22_TemplateMethod/ThaiNgocQui_DPM225464_Pattern22_TemplateMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Pattern22_TemplateMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AbstractClass aA = new ConcreteClassA();
+            aA.TemplateMethod();
+            AbstractClass aB = new ConcreteClassB();
+            aB.TemplateMethod();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ ThaiNgocQui_DPM225464_22_TemplateMethod/ThaiNgocQui_DPM225464_Real22_TemplateMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiNgocQui_DPM225464_Real22_TemplateMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataAccessor categories = new Categories();
+            categories.Run(5);
+            DataAccessor products = new Products();
+            products.Run(3);
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+}
